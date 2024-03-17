c26b483fd1628d03f249ede20d7fc52418b633b4
PavlenkoDm <dmpavlenko07@gmail.com>
2024-03-17 23:00:33 +0300

    ADD: setInterval in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  setInterval(() => {
+    return null;
+  }, 1500000);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
