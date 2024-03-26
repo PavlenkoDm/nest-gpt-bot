b580262881e3040758309b9b26bd4ec1abd3a508
PavlenkoDm <dmpavlenko07@gmail.com>
2024-03-26 21:11:07 +0300

    ADD: Dockerfile .dockerignore

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   setInterval(() => {
-    return null;
-  }, 600000);
+    console.log('Status OK. Keep working...');
+  }, 1500000);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
