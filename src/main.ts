fdcde0ff52755d7e43be5e17ddba4aebfcd486e9
PavlenkoDm <dmpavlenko07@gmail.com>
2024-04-15 21:50:23 +0300

    UPD: vercel.json

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  setInterval(() => {
-    console.log('Status OK. Keep working...');
-  }, 1500000);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
